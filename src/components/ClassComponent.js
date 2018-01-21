972328c8cd8c18e2ceb7dc51b8563d7d000f757f
Nasirilahi <nasirilahi.ilahi2@yahoo.com>
2018-01-21 12:40:02 +0530

    life cycle methods in details

--- src/components/ClassComponent.js
+++ src/components/ClassComponent.js
@@ -9,46 +9,49 @@ import {
 import PersonInfo from './Person';
 
 class ES6ClassComponent extends Component{
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {
             user:{
-                firstName:'abc'
+                firstName:this.props.firstName
             }
         };
-        console.log('constructor');
+        console.log('constructor_child');
 
     }
+    // On time execute when component has been instantiated
     componentWillMount(){
-        console.log('componentWillMount');
+        console.log('componentWillMount_child');
     }
+    // execute just after render when component has been mount on dom tree
     componentDidMount() {
-        console.log('componentDidMount');
+        console.log('componentDidMount_child');
     }
     componentWillReceiveProps(nextProps){
-        console.log('componentWillReceiveProps',nextProps);
+        this.setState({user: {firstName: nextProps.firstName}})
+        console.log('componentWillReceiveProps_child',nextProps);
     }
     shouldComponentUpdate(nextProps){
-        console.log('shouldComponentUpdate',nextProps);
+        console.log('shouldComponentUpdate_child',nextProps);
         return true;
     }
     componentWillUpdate(){
-        console.log('componentWillUpdate');
+        console.log('componentWillUpdate_child');
     }
     componentDidUpdate(){
-        console.log('componentDidUpdate');
+        console.log('componentDidUpdate_child');
     }
     componentWillUnmount() {
-        console.log('componentDidMount');
+        console.log('componentDidMount_child');
     }
-    onPress = () =>{
-        this.setState({user:{firstName :'xyz'}});
-    }
+
+
     render(){
-        console.log('render');
+        console.log('render_child');
         return(
         <View style={{marginBottom:50,}}>
-            <PersonInfo firstName={this.state.user.firstName} onPress={this.onPress} /> 
+            <Text>First name : {this.state.user.firstName}</Text>
+
         </View>
         );
     }
--- src/index.js
+++ src/index.js
@@ -2,20 +2,77 @@ import React from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+    TouchableOpacity
 } from 'react-native';
 import PersonInfo from './components/Person';
 import ClassComponent from './components/ClassComponent';
 import ES5ClassComponent from './components/ES5ClassComponent';
+// moment
+export default class index extends React.Component
+{
+    constructor(props){
+        super(props);
+        this.state = {
+            user:{
+                firstName:this.props.firstName,
+                time: '',
+            }
+        };
+        console.log('constructor_parent');
 
-export default function(){
-return(
-     <View style={styles.container}>
-          <ClassComponent />
-          {/*<ES5ClassComponent />*/}
-          {/*<PersonInfo firstName={'asdbasd'}  />*/}
-      </View>
-)
+    }
+    // On time execute when component has been instantiated
+    componentWillMount(){
+        setTimeout(()=> {
+            this.setState({time: this.state.time + 1 })
+        },1000)
+        console.log('componentWillMount_parent');
+    }
+    // execute just after render when component has been mount on dom tree
+    componentDidMount() {
+        console.log('componentDidMount_parent');
+    }
+    componentWillReceiveProps(nextProps){
+        // this.setState({user: {firstName: nextProps.firstName}})
+        console.log('componentWillReceiveProps_parent',nextProps);
+    }
+    shouldComponentUpdate(nextProps){
+        console.log('shouldComponentUpdate_parent',nextProps);
+        return true;
+    }
+    componentWillUpdate(){
+        console.log('componentWillUpdate_parent');
+    }
+    componentDidUpdate(){
+        console.log('componentDidUpdate_parent');
+    }
+    componentWillUnmount() {
+        console.log('componentDidMount_parent');
+    }
+
+    state = {
+        firstName: 'abc'
+    }
+    onPress = () =>{
+        this.setState({firstName :'xyz'});
+    }
+render(){
+    console.log('render_parent');
+    return(
+        <View style={styles.container}>
+            <ClassComponent firstName={this.state.firstName}  />
+            <TouchableOpacity
+                onPress={this.onPress}
+                style={{padding:20,backgroundColor:'red'}}
+            >
+                <Text>Change me </Text>
+            </TouchableOpacity>
+            {/*<ES5ClassComponent />*/}
+            {/*<PersonInfo firstName={'asdbasd'}  />*/}
+        </View>
+    )
+}
 }
 
 const styles = StyleSheet.create({
