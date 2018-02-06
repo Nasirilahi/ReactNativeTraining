963f4be842c103c482a5a198fe688559996e1aad
Nasirilahi <nasirilahi.ilahi2@yahoo.com>
2018-02-06 21:38:34 +0530

    added type checking and static type checking

--- index.ios.js
+++ index.ios.js
@@ -11,21 +11,18 @@ import {
   Text,
   View
 } from 'react-native';
+import Greeting from './src/Greetings'
+
 
 export default class reactnativeTraining extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <Greeting name="abc" />
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+
       </View>
     );
   }
